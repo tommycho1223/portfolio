eeceeae4651ca5e5f3aebec6a98883f56f5bcb68
Tommy <tommycho1223@gmail.com>
2025-01-23 14:16:03 -0800

    Editing global.js

--- global.js
+++ global.js
@@ -3,3 +3,6 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+const navLinks = $$("nav a");
+console.log(navLinks);
