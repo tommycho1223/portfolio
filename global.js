35a7c398bb2e3a29887e51a16060f2c7ababc2cd
Tommy <tommycho1223@gmail.com>
2025-01-24 16:49:24 -0800

    Working on step 3.1

--- global.js
+++ global.js
@@ -4,16 +4,47 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
+let navLinks = $$("nav a");
 console.log(navLinks);
 
 let currentLink = navLinks.find(
     (a) => a.host === location.host && a.pathname === location.pathname
-  );
+);
   
-  console.log(currentLink);
+console.log(currentLink);
 
-  if (currentLink) {
+if (currentLink) {
     // or if (currentLink !== undefined)
     currentLink.classList.add('current');
+}
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/tommycho1223', title: 'My GitHub' },
+    { url: 'cv/', title: 'Resume' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
   }
+
+  const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+  for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+  
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+      url = '../' + url;
+    }
+  
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
+  
