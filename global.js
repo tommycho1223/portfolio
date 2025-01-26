4ecf5d29d9c379d6d8f5d2eeab1dc2156803bc70
Tommy <tommycho1223@gmail.com>
2025-01-25 16:20:32 -0800

    Editing dark and light mode

--- global.js
+++ global.js
@@ -50,7 +50,7 @@ document.body.insertAdjacentHTML(
   <label class="color-scheme">
     Theme:
     <select>
-      <option value="light dark">Automatic</option>
+      <option value="light dark">Automatic (Dark)</option>
       <option value="light">Light</option>
       <option value="dark">Dark</option>
     </select>
