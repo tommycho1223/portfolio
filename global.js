b469c48014c31dbf3a74e94deeeb4763da433b02
Tommy <tommycho1223@gmail.com>
2025-01-31 09:28:55 -0800

    doing 3.1 in lab 3

--- global.js
+++ global.js
@@ -4,12 +4,12 @@ function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a"); // Get all navigation links
-console.log(navLinks); // Debugging: Check if we got the correct links
+// const navLinks = $$("nav a"); // Get all navigation links
+// console.log(navLinks); // Debugging: Check if we got the correct links
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-);
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+// );
 
 console.log(currentLink); // Debugging: Check if we found the correct link
 
