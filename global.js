3bfd8050b1fa7593093003a9dbb469243b9ef567
Tommy <tommycho1223@gmail.com>
2025-01-31 03:23:07 -0800

    Trying step 2.1 and 2.2 in lab 3

--- global.js
+++ global.js
@@ -3,3 +3,12 @@ console.log("IT'S ALIVE!");
 function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
+
+const navLinks = $$("nav a"); // Get all navigation links
+console.log(navLinks); // Debugging: Check if we got the correct links
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+console.log(currentLink); // Debugging: Check if we found the correct link
